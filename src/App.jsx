10975309f38c2c80abb897ffc35cc474aff21bf4
Bozidar Kuzmanovic <bozidarkuzmanovic@Bozidars-MacBook-Air.local>
2024-12-27 18:51:46 +0100

    clean install

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import './assets/css/styles.css'
+import './assets/js/script'
+import Carousel from './components/Carousel'
+import Contact from './components/Contact'
+import Faq from './components/Faq'
+import Features from './components/Features'
+import Locations from './components/Locations'
+import Logos from './components/Logos'
+import Nav from './components/Nav'
+import Showcase from './components/Showcase'
+import Testimonials from './components/Testimonials'
+import Transfer from './components/Transfer'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Nav />
+    <Showcase />
+    <Logos />
+    <Features/>
+    <Carousel />
+    <Transfer />
+    <Testimonials />
+    <Faq />
+    <Contact/>
+    <Locations /> 
     </>
   )
 }
